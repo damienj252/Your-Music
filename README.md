f8b2e7fd60d116b3ea77da9b8e5baeee22fada71
[damienj252] <damien@Damiens-MacBook-Pro.local>
2016-11-25 13:47:26 +0000

    Fourth Commit with three webpages

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#Your-Music
-# Your-Music
+#Your-Music is a webpage application that allows the user store the name of their favorite artist and songs in a data base using couchDB.
+You can search your favorite songs and artists using the navigation bar.
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-#Originally adapted from:
-# https://github.com/data-representation/example-project/blob/master/webapp.py
+# Author: Damien Joyce
+# Date: 
+
+# Originally adapted from: https://github.com/data-representation/example-project/blob/master/webapp.py
 
 import flask as fl
+
 import itertools as it
 
 app = fl.Flask(__name__)
@@ -12,8 +15,35 @@ def root():
 
 @app.route("/perms", methods=["GET", "POST"])
 def perms():
-	perms = [''.join(p) for p in it.permutations(fl.request.values["userinput"])]
+	perms = [''.join(p) for p in it.permutations(fl.request.values["artistinput"])]
 	return '\n'.join(perms)
+#-------------------------------------------------------------------------------------------------------------------------------
+class couchdb.client.Server(url='http://localhost:5984/', full_commit=True, session=None)
+server = Server() # connects to the local_server
+remote_server = Server('http://example.com:5984/')
+secure_remote_server = Server('https://username:password@example.com:5984/')
+
+db = server.create('music')
+db.name
+#-------------------------------------------------------------------------------------------------------------------------------
+class couchdb.client.Database(url, name=None, session=None)
+server = Server()
+db = server.create('music')
+
+doc_id, doc_rev = db.save({'artist': 'Chris Malinchak', 'song': 'Wonderful'})
+
+doc = db[doc_id]
+doc
+
+doc.id, doc.rev
+
+doc['artist']
+doc['song']
+
+doc['artist'] = artistinput
+doc['song'] = songinput
+
 
 if __name__ == "__main__":
     app.run()
+    
